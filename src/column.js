22ee8ffe08d6627f096f1b4fed86c48aa3b2ca11
superstark01 <superstark02@gmail.com>
2021-08-13 07:28:15 +0530

    123

--- src/column.js
+++ src/column.js
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 import { Droppable } from 'react-beautiful-dnd';
 import Task from './task';
 import { rdb } from './firebase'
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const Container = styled.div`
   margin: 8px;
@@ -27,21 +33,59 @@ const TaskList = styled.div`
 
 export default class Column extends React.Component {
 
-  addItem = (id,taskIds) => {
+  state = {
+    content: null,
+    open: false,
+    size: 0
+  }
+
+  addItem = (id, taskIds) => {
     var i = parseInt(taskIds.length) + 1;
-    taskIds.push("task-1"+i);
+    taskIds.push("task-1" + i);
     rdb.ref("/columns").child(id.toString()).child('taskIds').set(taskIds);
 
-    rdb.ref("/tasks").child("task-1"+taskIds.length).set({
-      id: "task-1"+taskIds.length,
+    rdb.ref("/tasks").child("task-1" + taskIds.length).set({
+      id: "task-1" + taskIds.length,
       content: "New Task Created"
     });
+  }
+
+  onDelete = (id) => {
+    var colOrder = this.props.initialData.columnOrder
+    var index = colOrder.indexOf(id)
+    colOrder.splice(index, 1);
+    rdb.ref().child('columnOrder').set(colOrder);
+
+    rdb.ref().child('columns').child(id).remove();
+  }
+
+  editItem = (id) => {
+    this.setState({open:id})
+  }
+
+  editItemUtil = (id) => {
+    rdb.ref("/columns").child(id).update({
+      title: this.state.content
+    }).then(res => {
+      this.setState({ content: "" });
+      this.setState({ open:false });
+    });
+  }
+
+  componentDidUpdate(){
+    
   }
 
   render() {
     return (
       <Container>
-        <Title>{this.props.column.title}</Title>
+        <div style={{ display: "flex", justifyContent: "space-between" }} >
+          <Title>{this.props.column.title}</Title>
+          <div style={{ padding: "8px" }} >
+            <img style={{ marginRight: "5px", cursor: "pointer" }} src="https://img.icons8.com/material-outlined/24/000000/pencil--v1.png" onClick={() => { this.editItem(this.props.column.id) }} width="15px" />
+            <img style={{ cursor: "pointer" }} src="https://img.icons8.com/material-outlined/24/000000/delete-sign.png" onClick={() => { this.onDelete(this.props.column.id) }} width="15px" />
+          </div>
+        </div>
         <Droppable droppableId={this.props.column.id}>
           {provided => (
             <TaskList ref={provided.innerRef} {...provided.droppableProps}>
@@ -60,6 +104,26 @@ export default class Column extends React.Component {
           <img width="15px" src="https://img.icons8.com/material-outlined/24/000000/add.png" />
           Add Item
         </div>
+
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"I hope you like the platform"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Enter your bucket name: <input value={this.state.content} onChange={(e) => { this.setState({ content: e.target.value }) }} />
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => { this.editItemUtil(this.state.open) }} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+
       </Container>
     );
   }
